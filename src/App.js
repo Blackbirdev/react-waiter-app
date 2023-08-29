5e0bc9095845e79422861a720565bcfc2d471269
Bartłomiej Kos <bart.blackbird@gmail.com>
2023-08-29 19:06:36 +0200

    Add routes for homepage, table and error page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Container } from "react-bootstrap";
+import { Route, Routes } from "react-router";
+import Homepage from "./components/pages/Homepage";
+import Table from "./components/pages/Table";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
     <div>
       <Container>
         <h1>Hello World</h1>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/table/:id" element={<Table />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </Container>
     </div>
   );
---
+++ src/components/pages/Homepage.js
@@ -0,0 +1,9 @@
+const Homepage = () => {
+    return (
+        <>
+            <h1>Homepage: All tables</h1>
+        </>
+    )
+}
+
+export default Homepage;
